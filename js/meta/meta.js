2a394fbee55fdec4e29772081faa654ccbf5bc6e
Oliver Sevilla <52464345+oliversevilla@users.noreply.github.com>
2019-10-04 12:09:41 -0500

    Generación de PDF y mejoras en gráficas

--- js/meta/meta.js
+++ js/meta/meta.js
@@ -10,10 +10,12 @@ function meta()
 //subir archivo al servidor
 meta.prototype.uploadURL = function(url){
     var este=this;
+    url=$.trim(url);
+    url=url.replace(/\t+/g,'');
     if(url!="")
     {
         var params = {            
-            "url": url
+            "url": $.trim(url)
         };
         $.ajax({
             data:params,
@@ -27,7 +29,8 @@ meta.prototype.uploadURL = function(url){
                 if(response>0) {
                     $("#oa_id").val(response);//asignar el oa_id para futuras consultas
                     showMsj("Archivos subidos exitosamente.<br />");
-                    $("#subio").css("visibility","visible");                    
+                    $("#subio").css("visibility","visible");
+                    $("#btnCalMetNew").removeClass("disabled");
                 }
                 if(response==-1) {
                     showMsj("El archivo de la URL indicada no contiene el est&aacute;ndar Dublin Core ni LOM.<br />");
@@ -128,16 +131,38 @@ meta.prototype.get = function(oa){
             }
         });
     }
-    else {
+    /****else {
         showMsj(miUnicode("Ingrese la dirección URL<br />"));    
         $("#subio").css("visibility","hidden");
-    }
+    }****/
 };
 
 //graficar metas
 meta.prototype.graficar = function(comp,cons,cohe){
+    
+    /*
+    var s1 = [comp,cons,cohe]; 
+    var ticks = ['COMPLETITUD '+comp, 'CONSISTENCIA'+cons, 'COHERENCIA'+cohe]; 
+    plot2 = $.jqplot('chart3', [s1,[],[]], { //give it extra blank series
+        seriesDefaults: { 
+           renderer: $.jqplot.BarRenderer, 
+           rendererOptions: { varyBarColor : true }, 
+           pointLabels: { show: true }, 
+           showLabel: true
+         }, 
+         series: [ {}, 
+                   {renderer: $.jqplot.LineRenderer}, // set our empty series to the lineRenderer, so the bar plot isn't padded for room
+                   {renderer: $.jqplot.LineRenderer},
+                   {renderer: $.jqplot.LineRenderer} ], 
+         //legend: { show: true, placement: 'outside', labels: ticks},  // give the legend the tick labels
+         axes:   { xaxis: { renderer: $.jqplot.CategoryAxisRenderer, ticks: ticks }}, 
+         yaxis:  {  tickOptions:{ formatString:'%.2f%' } }
+    });
+    return;*/
+    
+    
     var compBarColor='#17BDB8',consBarColor='#17BDB8',coheBarColor='#17BDB8';
-    var line1 = [['Completitud', comp],['Consistencia', cons],['Coherencia', cohe]];
+    var line1 = [['Completitud '+parseFloat(comp).toFixed(2), comp],['Consistencia '+parseFloat(cons).toFixed(2), cons],['Coherencia '+parseFloat(cohe).toFixed(2), cohe]];
         
     //Colores barra Completitud
     if(parseFloat(comp)==1){
@@ -169,11 +194,13 @@ meta.prototype.graficar = function(comp,cons,cohe){
     if(parseFloat(cohe)<0.5){
         coheBarColor='#D97E7E';//rojo
     }
-    
+    /*
     $('#chart3').jqplot([line1], {
+        //title: 'Bar Chart with Point Labels', 
         seriesColors:[compBarColor, consBarColor, coheBarColor],
         seriesDefaults:{
             renderer:$.jqplot.BarRenderer,
+            pointLabels: { show: true,labels:['fourteen', 'thirty two', 'fourty one']},
             rendererOptions: {
                 // Set varyBarColor to tru to use the custom colors on the bars.
                 varyBarColor: true
@@ -185,19 +212,43 @@ meta.prototype.graficar = function(comp,cons,cohe){
             },
             yaxis: {
                 min:0,
-                max:1.0
+                max:1.0,
+                padMax:1.3
             }
         },
         highlighter:{
-            show:true,
+            show:false,
             tooltipContentEditor:tooltipContentEditor
-        },
+        }
+    });
+    */
+    var s1 = [parseFloat(comp),parseFloat(cons),parseFloat(cohe)];
+    var ticks = ['COMPLETITUD '+parseFloat(comp).toFixed(2), 'CONSISTENCIA '+parseFloat(cons).toFixed(2), 'COHERENCIA '+parseFloat(cohe).toFixed(2)]; 
+    plot2 = $.jqplot('chart3', [s1,[],[]], { //give it extra blank series
+    //$('#chart3').jqplot([line1], {
+        //title: 'Bar Chart with Point Labels', 
+        //seriesColors:[compBarColor, consBarColor, coheBarColor],
+        seriesDefaults: { 
+           renderer: $.jqplot.BarRenderer, 
+           rendererOptions: { varyBarColor : true }, 
+           pointLabels: { show: true }, 
+           showLabel: true
+         }, 
+         series: [ {}, 
+                   {renderer: $.jqplot.LineRenderer}, // set our empty series to the lineRenderer, so the bar plot isn't padded for room
+                   {renderer: $.jqplot.LineRenderer},
+                   {renderer: $.jqplot.LineRenderer} ], 
+         //legend: { show: true, placement: 'outside', labels: ticks},  // give the legend the tick labels
+         axes:   { xaxis: { renderer: $.jqplot.CategoryAxisRenderer, ticks: ticks }}, 
+         yaxis:  {  tickOptions:{ formatString:'%.2f%' } }
     });
 };
 
 //generar reporte de metas
-meta.prototype.rpt = function(oa){
-    var este=this;
+meta.prototype.rpt = function(oa,ext){
+    var este=this,vUrl='';
+    if(ext=='txt') vUrl='../../controlador/meta/coRptTxt.php';
+    if(ext=='pdf') vUrl='../../controlador/meta/coRptPdf.php';
     if(oa>0)
     {
         var params = {            
@@ -205,15 +256,16 @@ meta.prototype.rpt = function(oa){
         };
         $.ajax({
             data:params,
-            url:'../../controlador/meta/coRpt.php',
+            url:vUrl,
             type:'POST',
             //dataType:'JSON',
             beforeSend: function () {
                 showMsjSinBoton("Generando Reporte de M&eacute;tricas del OA... por favor espere<br />");
             },
             success:  function (response) {
-                hideMsj();
-                este.hash(response);
+                hideMsj();                
+                if(ext=='txt') este.txt(response);
+                if(ext=='pdf') este.pdf(response);
             },
             error : function(XMLHttpRequest, textStatus, errorThrown) {
                 alert(textStatus+". "+errorThrown);
@@ -226,17 +278,140 @@ meta.prototype.rpt = function(oa){
     }
 };
 
+//generar reporte de metas dado el repositorio
+meta.prototype.rptRepo = function(re_id){
+    
+    var table1 = tableToJson($('#datatable').get(0)),
+        cellWidth = 35,rowCount = -1,cellContents,leftMargin = 2,anchoCol=0,
+        topMargin0 = 12, topMargin1 = 22,topMargin2 = 52, topMarginTable = 55,headerRowHeight = 18,
+        rowHeight = 15,
+        l = {
+            orientation: 'l',unit: 'mm',format: 'a3',
+            compress: true,fontSize: 8,lineHeight: 1,
+            autoSize: true,printHeaders: true
+        };
+    
+    //crear objeto PDF
+    var doc = new jsPDF(l, '', '', '');
+    doc.setFontSize(22);
+    doc.setFontType('bold')
+    doc.text(2, topMargin0, miUnicode('Reporte de OAs por Repositorio'));
+    
+    doc.setProperties({
+        title: 'Reporte de Repositorio',subject: 'Reporte de Repositorio y sus Metricas',
+        author: 'Oliver Sevilla',keywords: 'reporte repositorio metricas',
+        creator: 'Oliver Sevilla'
+    });
+
+    doc.cellInitialize();
+    
+    //imprimircabecera de la tabla
+    doc.margins = 1;
+    doc.setFont("helvetica");
+    doc.setFontType("bold");
+    doc.setFontSize(12);
+    doc.cell(leftMargin, topMargin1, 9, headerRowHeight, "#", -1);
+    doc.cell(leftMargin, topMargin1, 57, headerRowHeight, "REPOSITORIO", -1);
+    doc.cell(leftMargin, topMargin1, 100, headerRowHeight, "TITULO", -1);
+    doc.cell(leftMargin, topMargin1, 100, headerRowHeight, "KEYWORDS", -1);
+    doc.cell(leftMargin, topMargin1, 34, headerRowHeight, "ESTANDAR", -1);
+    doc.cell(leftMargin, topMargin1, 100, headerRowHeight, "RESULTADOSR", -1);
+    
+    $.each(table1, function (i, row)
+    {
+        //imprimir cada fila
+        rowCount++;
+        $.each(row, function (j, cellContent) {
+            //definir ancho de columna segun corresponda
+            anchoCol=100;
+            if(j=='#') anchoCol=9;
+            if(j=='repositorio') anchoCol=57;
+            if(j==miUnicode('estándar')) anchoCol=34;
+            //imprimir columnas
+            doc.margins = 1;
+            doc.setFont("courier ");
+            doc.setFontType("normal ");
+            doc.setFontSize(11);
+            doc.cell(leftMargin, topMargin2, anchoCol, rowHeight, cellContent, i);
+        })
+    })
+    doc.save('Reporte de OAs por Repositorio.pdf');
+};
+
 //cambiar tootltiptext al pasar mouse sobre las graficas
 function tooltipContentEditor(str, seriesIndex, pointIndex, plot) {  
     return plot.series[seriesIndex]["label"] + ", " + plot.data[seriesIndex][pointIndex];
 }
 
+function tableToJson(table) {
+    var data = [];
 
+    // first row needs to be headers
+    var headers = [];
+    for (var i=0; i<table.rows[0].cells.length; i++) {
+        headers[i] = table.rows[0].cells[i].innerHTML.toLowerCase().replace(/ /gi,'');
+    }
 
+    // go through cells
+    for (var i=1; i<table.rows.length; i++) {
+        var tableRow = table.rows[i];
+        var rowData = {};
+        for (var j=0; j<tableRow.cells.length; j++) {
+            rowData[ headers[j] ] = tableRow.cells[j].innerHTML;
+        }
+        data.push(rowData);
+    }       
+    return data;
+}
 
 //descarga de archivos reportes
-meta.prototype.hash= function(str)
-{      
+//descarga de archivos reportes
+meta.prototype.pdf= function(str)
+{     
+    var pageStart = 20;
+    var step = 5;
+    var line = 30;
+    
+    var pdf = new jsPDF();
+    pdf.setFontSize(22);
+    pdf.setFontType('bold')
+    pdf.text(20, 20, miUnicode('Reporte de Métricas Inconsistentes'));
+    pdf.setFontType('normal')
+    pdf.setFontSize(12);
+    
+    //imprimir str con multilinea si se requiere
+    var splitTitle = pdf.splitTextToSize(str, 180);
+    for (var i = 0; i < splitTitle.length; i++) {
+        pdf.text(splitTitle[i], 20, line);
+        if (line >= 275) {
+            pdf.addPage();
+            line = pageStart;
+        }
+        line = line + step;
+    }
+    
+    //pdf.text(20,30,str);
+    pdf.setFontSize(10);
+    pdf.setFont('times')
+    pdf.setFontType('italic')
+    pdf.setTextColor(255,0,0);
+    pdf.text(20,pdf.internal.pageSize.height - 10,'Por problemas de compatibilidad con el navegador se pueden omitir caracteres especiales como tildes');
+    pdf.save('Reporte de Análisis.pdf');
+    
+    /*html2canvas(document.body,{
+        onrendered:function(canvas){
+            var img=canvas.toDataURL("image/png");
+            var doc = new jsPDF();
+            doc.addImage(img,'JPEG',20,20);
+            doc.save('test.pdf');
+        }
+    });*/
+    
+    ////this.descargarArchivo(new Blob([str.toString()], {type: 'application/pdf'}), 'ReporteMetricas.pdf');    
+}
+
+meta.prototype.txt= function(str)
+{     
     this.descargarArchivo(new Blob([str.toString()], {type: 'text/plain'}), 'ReporteMetricas.txt');    
 }
 
@@ -294,7 +469,8 @@ meta.prototype.validateUploadFileTXT = function(){
                         }
                         else{ 
                             showMsj("Archivo subido exitosamente.<br />");
-                            $("#subio").css("visibility","visible");//visto en FE                        
+                            $("#subio").css("visibility","visible");//visto en FE
+                            $("#btnCalMetBdd").removeClass("disabled");
                         }
                     },
                     error : function(XMLHttpRequest, textStatus, errorThrown) {
@@ -346,13 +522,18 @@ meta.prototype.calcularTXT = function(){
                 }
             });
         }
-        else {
+        /****else {
             showMsj(miUnicode("El archivo TXT aún no han sido subido al servidor<br />"));    
             $("#subio").css("visibility","hidden");
-        }        
+        }****/
     }    
-    else {
+    /****else {
         showMsj(miUnicode("El archivo TXT no han sido seleccionado (.txt)<br />"));    
         $("#subio").css("visibility","hidden");
-    }        
+    }****/    
+};
+
+meta.prototype.changeRepo= function()
+{
+    window.location.href='frmRptOA.php?re_id='+$("#re_id").val();
 };
